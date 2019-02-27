1b794aeeba7696ff7b3bbae5a251e2d78810b99b
pangyuteng <pangyuteng@gmail.com>
2019-02-26 23:38:03 -0800

    periodic commit

--- examples/celery-rabbitmq/task/run_tasks.py
+++ examples/celery-rabbitmq/task/run_tasks.py
@@ -19,13 +19,14 @@ import syslog
 import time
 print('yolo4')
 from celery_conf import add
+import celeryconfig
 print('yolo5')
 while True:
     x = random.randint(1, 10)
     y = random.randint(1, 10)
     print('hola!!!',time.time())
     add.apply_async(args=(x, y))
-    print('laho!!!',time.time())
+    print('laho!!!',time.time(),celeryconfig.CELERY_BROKER_URL)
     #break
     #res = add.delay(x, y)
     #time.sleep(5)
